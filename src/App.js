812aa026ddb14ff432c6c6aef7b11bb7333a6af1
alanymara <alanymara@gmail.com>
2022-01-25 15:34:15 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
+import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
+        <PlanetCard />
       </div>
     );
   }
