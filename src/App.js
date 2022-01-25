5a6226741f8d0663e9f0a85f341356856cc90805
alanymara <alanymara@gmail.com>
2022-01-25 16:15:23 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
 
 class App extends React.Component {
@@ -9,7 +8,6 @@ class App extends React.Component {
       <div>
         <Header />
         <SolarSystem />
-        <PlanetCard />
       </div>
     );
   }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { Planets.map(({ name, image }) => (
+          <PlanetCard key={ name } planetName={ name } planetImage={ image } />))}
       </div>
     );
   }
 }
-
 export default SolarSystem;
